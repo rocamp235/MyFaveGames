7a294539fbbb98fafb1292d4a5143672973ae745
rocamp235 <rocamp235@users.noreply.github.com>
2019-09-16 17:50:52 -0500

    MyFaveGames

--- MyFaves/MasterViewController.swift
+++ MyFaves/MasterViewController.swift
@@ -1,90 +1,166 @@
 //
 //  MasterViewController.swift
-//  MyFaves
+//  MyFaves2
 //
-//  Created by Roberto Ocampo on 9/16/19.
-//  Copyright © 2019 Roberto Ocampo. All rights reserved.
-//
+//  Created by Charles Konkol on 9/11/17.
+//  Copyright © 2018 RockValleyCollege. All rights reserved.
+//  Updated 9/9/18
 
 import UIKit
 
 class MasterViewController: UITableViewController {
-
+    
     var detailViewController: DetailViewController? = nil
-    var objects = [Any]()
-
-
+    // 1) comment out var objects line
+    //var objects = [Any]()
+    
+    // 2) declare and initialize two arrays
+    var ListOfPhotos:[UIImage] = []
+    var ListOfFavs:[String]?
+    var FavDetails:[String]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        navigationItem.leftBarButtonItem = editButtonItem
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        navigationItem.rightBarButtonItem = addButton
-        if let split = splitViewController {
+        // Do any additional setup after loading the view, typically from a nib.
+        // 3) Comment out below lines
+        //        navigationItem.leftBarButtonItem = editButtonItem
+        //
+        //        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
+        //        navigationItem.rightBarButtonItem = addButton
+        //        if let split = splitViewController {
+        //            let controllers = split.viewControllers
+        //            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        //        }
+        
+        // 4) Add items to 2 arrays
+        //Menus
+        ListOfPhotos = [
+            UIImage(named: "hs.jpg")!,
+            UIImage(named: "csgo.jpg")!,
+            UIImage(named: "overwatch.jpg")!,
+            UIImage(named: "gtav.jpg")!,
+            UIImage(named: "wow.jpg")!,
+            UIImage(named: "fifa.jpg")!,
+            UIImage(named: "mario.jpg")!,
+            UIImage(named: "mk.jpg")!,
+            UIImage(named: "civ.jpg")!,
+            UIImage(named: "nba2k.jpg")!
+            
+        ]
+        
+        ListOfFavs = ["Hearthstone","CS:GO","Overwatch", "GTA V", "World of Warcraft", "Fifa 20", "Super Mario Bros.", "Mortal Kombat 11", "Civilization V", "NBA 2K20"]
+        FavDetails = ["https://playhearthstone.com/en-us/","https://blog.counter-strike.net/","https://playoverwatch.com/en-us/", "https://www.rockstargames.com/V/restricted-content/agegate/form?redirect=https%3A%2F%2Fwww.rockstargames.com%2FV%2F&options=&locale=en_us","https://worldofwarcraft.com/en-us/wowclassic", "https://www.ea.com/games/fifa/fifa-20","https://supermariobros.io/","https://www.mortalkombat.com/", "https://civilization.com/civilization-5/", "https://nba.2k.com/en-US/"]
+        
+        if let split = self.splitViewController {
             let controllers = split.viewControllers
-            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
     }
-
-    @objc
-    func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-
+    
+    // 5) Comment out func insertNewObject.
+    //Select from line finc InsertNewObject to }
+    //and use shortcut command: [Command] [/] to comment all lines
+    
+    //    func insertNewObject(_ sender: Any) {
+    //        objects.insert(NSDate(), at: 0)
+    //        let indexPath = IndexPath(row: 0, section: 0)
+    //        tableView.insertRows(at: [indexPath], with: .automatic)
+    //    }
+    
     // MARK: - Segues
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                // 6) replace code with below code until line 75
+                let object = FavDetails![indexPath.row]
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
-                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.weburl = (object as AnyObject as? String)!
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem
                 controller.navigationItem.leftItemsSupplementBackButton = true
+                controller.navigationItem.title = ListOfFavs![indexPath.row]
             }
         }
     }
-
+    
     // MARK: - Table View
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        // 7) Replace objects.count with ListOfFavs!.count
+        return ListOfFavs!.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        // 8) Replace code with below code until }
+        cell.textLabel!.text = ListOfFavs![indexPath.row]
+        let imagename:UIImage = ListOfPhotos[indexPath.row]
+        cell.imageView?.image = imagename.resize(maxWidthHeight: 30)
+        cell.detailTextLabel?.text = ">>"
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return true
+        //9) Make below false
+        return false
     }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-        }
-    }
-
-
+    
+    // 10) Comment out override func tableView(tableView  - shortcut command: [Command] [/]
+    
+    //    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    //        if editingStyle == .delete {
+    //            objects.remove(at: indexPath.row)
+    //            tableView.deleteRows(at: [indexPath], with: .fade)
+    //        } else if editingStyle == .insert {
+    //            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+    //        }
+    //    }
+    
+    
 }
-
+//10 Add image extension
+extension UIImage {
+    
+    func resize(maxWidthHeight : Double)-> UIImage? {
+        
+        let actualHeight = Double(size.height)
+        let actualWidth = Double(size.width)
+        var maxWidth = 0.0
+        var maxHeight = 0.0
+        
+        if actualWidth > actualHeight {
+            maxWidth = maxWidthHeight
+            let per = (100.0 * maxWidthHeight / actualWidth)
+            maxHeight = (actualHeight * per) / 100.0
+        }else{
+            maxHeight = maxWidthHeight
+            let per = (100.0 * maxWidthHeight / actualHeight)
+            maxWidth = (actualWidth * per) / 100.0
+        }
+        
+        let hasAlpha = true
+        let scale: CGFloat = 0.0
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: maxWidth, height: maxHeight), !hasAlpha, scale)
+        self.draw(in: CGRect(origin: .zero, size: CGSize(width: maxWidth, height: maxHeight)))
+        
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        return scaledImage
+    }
+    
+}
